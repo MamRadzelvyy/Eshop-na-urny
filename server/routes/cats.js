6d19c4b2507498270e770e2e30e658e999c76ef4
MamRadZelvyy <robisdan@seznam.cz>
2024-11-10 18:22:37 +0100

    zkouška

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -5,9 +5,6 @@ const catsController = require("../controllers/cats");
 
 router.get("/", catsController.getAllCats);
 
-//localhost:3000/cats/5sa4d949qw86d5sa4d6sa
-//req.params.id
-
 router.get("/:id", catsController.getCatById);
 
 router.delete("/:id", catsController.deleteCat);
